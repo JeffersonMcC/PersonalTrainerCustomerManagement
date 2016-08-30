bfa0bd09590a96bb4b1f497dd63011da43970551
JeffersonMcC <youremail@who.com>
2016-08-30 00:15:42 -0400

    Some visual changes

--- app/src/main/java/com/bignerdranch2nded/android/personaltrainer/MainActivity.java
+++ app/src/main/java/com/bignerdranch2nded/android/personaltrainer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bignerdranch2nded.android.personaltrainer;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setIcon(R.mipmap.ic_launcher);
     }
 }
